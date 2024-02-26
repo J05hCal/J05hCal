1473fdb33770b0120949d93afc36654df10ca55d
Isaiah714 <Ip714@csu.fullerton.edu>
2024-02-25 20:27:09 -0800

    Moment of truth Mr. Yellow Man.

--- piCalculation.cpp
+++ piCalculation.cpp
@@ -26,67 +26,73 @@ int main()
     cin >> input;
   }
 
-  // If it is iteration, ask how many iterations and call the Iterate function.
+// If it is iteration, ask how many iterations and call the Iterate function.
+if (input == 'I' || input == 'i')
+{
   cout << "How many interations would you like?  ";
   cin >> iter_amount;
   Iterate(iter_amount);
 
-     
-    
+}
 
   // If it is approximation, ask the number of approximation and call thw DecimalApprox function
-
-  // Prompt the user of decimal approximation
+if (input == 'A' || input == 'a')
+{
+   // Prompt the user of decimal approximation
   cout << "How many decimals of approximation: ";
   cin >> decimal_aprox;
 
   // Call the DecimalApprox function
   DecimalApprox(decimal_aprox);
-  
+}
+
   return 0;
 } // end of "main"
-
+/*=================================================================================================*/
 void Iterate (int iter)
 {
-  ??? // variables
+
+  // variable
+  double pi{0.0};
 
   // do a loop to calculate pi based on the number of iterations
-  ???
+  for (size_t i{0}; i < iter; i++)
   {
-    ???
+    double pi{pow(-1, iter)/(2 * iter + 1)};
   }
 
   // Call the DisplayPi function.
   // default to 5 digit significance for iteration
-  DisplayPi(5, ???);
+  DisplayPi(5, pi);
 
 } // end of "Iterate"
 
 void DecimalApprox(int approx)
 {
-  ??? // variables
-  double piVal ??? // Pi value
-  double previousPi = piVal; // Previous pi value
+  int i{0};
+  double piVal{3.14}; // Pi value
+  double previousPi{piVal}; // Previous pi value
 
   // loop to calculate pi based on the approx value of signif. digits.
   do{
-      ??? // calculate PI
+      double pi{pow(-1, approx)/(2 * approx + 1)}; // calculate PI
 
       // check if the significance has changed within 1 decimal approx.
       //if so break out of the loop
       if (abs(piVal - previousPi) <= pow(10.0, -1.0 * approx))
       {
-        ???
+        break;
       }
 
       // update previousPi and increment i
-      ???
-      ???
-  } while (???);
+      
+      i++;
+      previousPi{pi};
+  } while (i <= approx);
 
   // Call the DisplayPi function.
   // default to approx digit significance for iteration
-  DisplayPi(???, ???);
+  DisplayPi(approx, previousPi);
 
 } // end of "DecimalApprox"
 
@@ -95,8 +101,8 @@ void DisplayPi (int signif, double pi)
   // settings for digit precision
   cout.setf(ios::showpoint);
   cout.setf(ios::fixed);
-  cout.precision(???);
+  cout.precision(signif);
 
   //display the value of pi
-  cout << "The value of Pi is: " << ??? << endl << endl;
+  cout << "The value of Pi is: " << pi << endl << endl;
 } // end of "DisplayPi"
