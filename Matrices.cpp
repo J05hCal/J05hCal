ce18a42ed08006e1449be1323521c0cabcf44038
J05hCal <160571503+J05hCal@users.noreply.github.com>
2024-03-17 00:04:43 -0700

    Update Matrices.cpp

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void InitMatrix(int intVal)
-void 
+void InitMatrix(int i, int intVal[][9], int numRows) //User enter's values
+{
+  int row;
+  int col;
+  
+  cout << "ENTER 9 INTEGER VALUES FOR THE FIRST MATRIX: " << endl;
+  cin >> intVal[][0];
+  
+  for (row = 0; row < numRows; ++row)
+    {
+      for (col = 0; col < 3; ++col)
+        {
+          cout << setw(3) << intVal[row][col];
+        }
+      cout << endl;
+    }
+}
+void DispMatrix() //Displays values
+{
+  set
+}
+void AddMatrix() //Adds up values
+void TMatrix()
+int DetMatrix()
 
 int main()
 {
-  cout << "ENTER 9 VALUES: " << endl;
+  
   cin >> intVal;
   return 0;
 }
