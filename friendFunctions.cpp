51563b02b1b44d5a730aca371048ef8e3f8c7a7e
J05hCal <160571503+J05hCal@users.noreply.github.com>
2024-04-17 15:18:09 -0700

    Create friendFunctions.cpp

---
+++ friendFunctions.cpp
@@ -0,0 +1,4 @@
+//A non-member function that is granted direct access to private data members in a class declaration
+//Put "friend" before the return type in the function header (not implementation)
+//Can also go in private section and still compile, but it is recommended to place in the public section
+
